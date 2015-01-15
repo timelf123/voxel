99ceb1f7af190c734352437a57e575e4889e22f5
Christoph Tavan <dev@tavan.de>
2015-01-15 19:04:34 +0100

    Install puppet 2.7.26 instead of 2.7.23

--- postinstall.default.sh
+++ postinstall.default.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wget -O /tmp/facter_2.7.3-1puppetlabs1_amd64.deb http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
-wget -O /tmp/puppet-common_2.7.23-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.23-1puppetlabs1_all.deb
-wget -O /tmp/puppet_2.7.23-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.23-1puppetlabs1_all.deb
+wget -O /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
+wget -O /tmp/puppet_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.26-1puppetlabs1_all.deb
 dpkg -i /tmp/facter_1.7.3-1puppetlabs1_amd64.deb
-dpkg -i /tmp/puppet-common_2.7.23-1puppetlabs1_all.deb
-dpkg -i /tmp/puppet_2.7.23-1puppetlabs1_all.deb
+dpkg -i /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb
+dpkg -i /tmp/puppet_2.7.26-1puppetlabs1_all.deb
 
 $ROOT_USERS=(
     leitmedium
