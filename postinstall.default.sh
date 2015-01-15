797dc1ef721f2364fed6e912972776ce94ddb47f
Christoph Tavan <dev@tavan.de>
2015-01-15 19:25:29 +0100

    Fix facter download name.

--- postinstall.default.sh
+++ postinstall.default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-wget -O /tmp/facter_2.7.3-1puppetlabs1_amd64.deb http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
+wget -O /tmp/facter_1.7.3-1puppetlabs1_amd64.deb http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
 wget -O /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
 wget -O /tmp/puppet_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.26-1puppetlabs1_all.deb
 dpkg -i /tmp/facter_1.7.3-1puppetlabs1_amd64.deb
