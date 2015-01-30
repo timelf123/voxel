95af1f1da33c7eeeb49513a826debe7512ede746
Christoph Tavan <dev@tavan.de>
2015-01-30 12:35:40 +0100

    Install hiera as well.

--- postinstall.default.sh
+++ postinstall.default.sh
@@ -3,9 +3,15 @@
 wget -O /tmp/facter_1.7.3-1puppetlabs1_amd64.deb http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
 wget -O /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
 wget -O /tmp/puppet_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.26-1puppetlabs1_all.deb
+wget -O /tmp/hiera_1.3.4-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/h/hiera/hiera_1.3.4-1puppetlabs1_all.deb
+wget -O /tmp/hiera-puppet_1.0.0-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/h/hiera-puppet/hiera-puppet_1.0.0-1puppetlabs1_all.deb
+
 dpkg -i /tmp/facter_1.7.3-1puppetlabs1_amd64.deb
 dpkg -i /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb
 dpkg -i /tmp/puppet_2.7.26-1puppetlabs1_all.deb
+dpkg -i /tmp/hiera_1.3.4-1puppetlabs1_all.deb
+dpkg -i /tmp/hiera-puppet_1.0.0-1puppetlabs1_all.deb
+
 rm -rf /tmp/*.deb
 
 ROOT_USERS=(
